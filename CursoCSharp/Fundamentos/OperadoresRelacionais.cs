f76fd562a4fb9488b48de5418a2862a599e7ed5a
KaikSAlves <souzaalveskaik@gmail.com>
2025-08-13 17:14:31 -0300

    OperadoresRelacionais

---
+++ CursoCSharp/Fundamentos/OperadoresRelacionais.cs
@@ -0,0 +1,20 @@
+﻿namespace CursoCSharp.Fundamentos;
+
+public class OperadoresRelacionais
+{
+    public static void Executar()
+    {
+        double nota = 6.0;
+        double notaDeCorte = 7.0;
+        
+        Console.WriteLine("Nota inválida? {0}", nota > 10.0);
+        Console.WriteLine("Nota inválida? {0}", nota < 0.0);
+        Console.WriteLine("Nota inválida? {0}", nota == 0.0);
+        Console.WriteLine("Nota inválida? {0}", nota != 0.0);
+        Console.WriteLine("Nota inválida? {0}", nota >= notaDeCorte);
+        Console.WriteLine("Nota inválida? {0}", nota <= notaDeCorte );
+        
+        
+        
+    }
+}
