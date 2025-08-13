e9dcfecc5e36728ffe729b17a97c6b1e781fe33e
KaikSAlves <souzaalveskaik@gmail.com>
2025-08-13 16:07:13 -0300

    Tipos por inferencia

---
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos;
+
+public class Inferencia
+{
+    public static void Executar()
+    {   
+        var nome = "Leonoardo";
+        Console.WriteLine(nome);
+    }
+}
