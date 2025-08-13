788501a9d2eedd217ef81a927c65bcdb66f5f114
KaikSAlves <souzaalveskaik@gmail.com>
2025-08-13 17:03:37 -0300

    Conversoes

---
+++ CursoCSharp/Fundamentos/Conversoes.cs
@@ -0,0 +1,39 @@
+﻿namespace CursoCSharp.Fundamentos;
+
+public class Conversoes
+{
+    public static void Executar()
+    {
+        int inteiro = 10;
+        double quebrado = inteiro; //conversao implicita
+        
+        Console.WriteLine("Valor quebrado: " + quebrado);
+
+        double nota = 9.7;
+
+        int notaTruncada = (int) nota; //casting
+        Console.WriteLine("Valor truncado: " + notaTruncada);
+        
+        Console.Write("Sua idade: ");
+        string idadeString = Console.ReadLine();
+        int idade = int.Parse(idadeString);
+        
+        Console.WriteLine("Sua idade: " + idade);
+
+        int idadeInteiro = Convert.ToInt32(idade); //outra forma de conversao explicita
+        
+        Console.Write("Digite o primeiro número: ");
+        string palavra = Console.ReadLine();
+        int numero;
+        int.TryParse(palavra, out numero); // o out é para que, caso não seja possivel converter 
+        //a variável ele joga um valor (por padrao 0) para ela
+        
+        Console.WriteLine(numero);
+
+        Console.Write("Digite o segundo número: ");
+        int.TryParse(Console.ReadLine(), out int numero2);  
+        
+        Console.WriteLine(numero2);
+
+    }
+}
--- CursoCSharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
+++ CursoCSharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72c51b287a905a4458751aba6807f1338e41d2fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00b2c0ed90c1c7bc2172aad5d05a7eb75c1d2a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
