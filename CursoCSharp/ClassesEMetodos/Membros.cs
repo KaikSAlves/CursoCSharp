85fda094d08e29107d58f4db3b8ec9ab803c99ff
KaikSAlves <souzaalveskaik@gmail.com>
2025-08-14 16:40:46 -0300

    Membros - Classes e Metodos

---
+++ CursoCSharp/ClassesEMetodos/Membros.cs
@@ -0,0 +1,13 @@
+﻿namespace CursoCSharp.ClassesEMetodos;
+
+public class Membros
+{
+    public static void Executar()
+    {
+        Pessoa sicrano = new Pessoa();
+        sicrano.Nome = "Renato";
+        sicrano.Idade = 21;
+        
+        Console.WriteLine($"{sicrano.Nome} tem {sicrano.Idade} anos!");
+    }
+}
