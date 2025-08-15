7e820fb2a7f556166a0632cbc8806e3d2dfc003a
KaikSAlves <souzaalveskaik@gmail.com>
2025-08-15 17:16:34 -0300

    Parametros Nomeados

---
+++ CursoCSharp/ClassesEMetodos/ParametrosNomeados.cs
@@ -0,0 +1,14 @@
+﻿namespace CursoCSharp.ClassesEMetodos;
+
+public class ParametrosNomeados
+{
+
+    public static void Formatar(int dia, int mes, int ano)
+    {
+        Console.WriteLine("{0:d2}/{1:d2}/{2}", dia, mes, ano);
+    }
+    public static void Executar()
+    {
+        Formatar(mes: 06, dia: 10, ano: 2000);
+    }
+}
--- CursoCSharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
+++ CursoCSharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+451078946db2f800c9e21b2ac7c5bcb2d06449cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26297d6d2babf0246b2e1b3b785e9e35457418e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
