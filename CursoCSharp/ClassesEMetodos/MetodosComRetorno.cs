f393dc73eede1b00d7b6f7b2415f3dae509ca1a8
KaikSAlves <souzaalveskaik@gmail.com>
2025-08-15 16:50:13 -0300

    Metodos com retorno #2

--- CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
+++ CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
@@ -6,7 +6,7 @@ public class CalculadoraComum
     {
         return c + d;
     }
-    
+
     public int Somar(int a, int b)
     {
         return a + b;
@@ -17,7 +17,7 @@ public class CalculadoraComum
         return a * b;
     }
 
-    public int Dividir(int a , int b)
+    public int Dividir(int a, int b)
     {
         return a / b;
     }
@@ -28,19 +28,71 @@ public class CalculadoraComum
     }
 }
 
+public class CalculadoraCadeia
+{
+    private int memoria;
+
+    public CalculadoraCadeia Somar(int a)
+    {
+        memoria += a;
+        return this;
+    }
+
+    public CalculadoraCadeia Subtracao(int a)
+    {
+        memoria -= a;
+        return this;
+    }
+
+    public CalculadoraCadeia Divisao(int a)
+    {
+        memoria /= a;
+        return this;
+    }
+
+    public CalculadoraCadeia Multiplicacao(int a)
+    {
+        memoria *= a;
+        return this;
+    }
+
+    public CalculadoraCadeia Imprimir()
+    {
+        Console.WriteLine(memoria);
+        return this;
+    }
+
+    public CalculadoraCadeia Limpar()
+    {
+        memoria = 0;
+        return this;
+    }
+
+    public int Resultado()
+    {
+        return memoria;
+    }
+}
+
 public class MetodosComRetorno
 {
     public static void Executar()
     {
         var calc = new CalculadoraComum();
-        
+
         Console.WriteLine("Informe dois valores inteiros para serem processados na calculadora: ");
         int.TryParse(Console.ReadLine(), out int v1);
         int.TryParse(Console.ReadLine(), out int v2);
-        
+
         Console.WriteLine($"Soma {v1} + {v2} : {calc.Somar(v1, v2)}");
         Console.WriteLine($"Divisão {v1} / {v2} : {calc.Dividir(v1, v2)}");
         Console.WriteLine($"Multiplicar {v1} * {v2} : {calc.Multiplicar(v1, v2)}");
         Console.WriteLine($"Subtração {v1} - {v2} : {calc.Subtrair(v1, v2)}");
+        
+        var CalculadoraCadeia = new CalculadoraCadeia();
+
+        CalculadoraCadeia.Somar(2).Somar(4).Imprimir().Multiplicacao(5).Imprimir().Limpar()
+            .Somar(2).Subtracao(2).Divisao(1).Imprimir();
+
     }
 }
--- CursoCSharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
+++ CursoCSharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5aefb7a9320a4625f0561df5d250ff9d8813735")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe6dc407cebea6ea61cf09b368ee166b4f3c7cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
