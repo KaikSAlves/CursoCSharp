7db8ede3a2651a3ae36eb7517267bf41e42fbc6a
KaikSAlves <souzaalveskaik@gmail.com>
2025-08-15 17:26:17 -0300

    Get e Set

---
+++ CursoCSharp/ClassesEMetodos/GetSet.cs
@@ -0,0 +1,57 @@
+﻿namespace CursoCSharp.ClassesEMetodos;
+
+public class Funcionario
+{
+    private string Nome;
+    private double Salario;
+
+    public Funcionario(string nome, double salario)
+    {
+        Nome = nome;
+        Salario = salario;
+    }
+
+
+    public Funcionario()
+    {
+        
+    }
+
+    public string getNome()
+    {
+        return Nome;
+    }
+
+    public double getSalario()
+    {
+        return Salario;
+    }
+
+    public void setSalario(double salario)
+    {
+        Salario = salario;
+    }
+
+    public void setNome(string nome)
+    {
+        Nome = nome;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome: {Nome}\n" +
+               $"Salario: {Salario}";
+    }
+}
+public class GetSet
+{
+    public static void Executar()
+    {
+        var func1 = new Funcionario();
+        
+        func1.setNome("KAik");
+        func1.setSalario(10000);
+
+        Console.WriteLine(func1);
+    }
+}
